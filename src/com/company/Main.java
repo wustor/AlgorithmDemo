ef4c8b9ac1949f0c6e45ed1bd32f243c90dc72a8
pangchao <15527283152@163.com>
2018-01-02 20:58:28 +0800

    修改main文件

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,6 +13,7 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        System.out.println("hello");
     }
 
     public static class MinStack {
